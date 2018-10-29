9c19c4715139cdd894ca76292b8ddca72690675c
hejack0207 <hejack0207@sina.com>
2018-10-29 22:36:06 +0800

    add plantuml-snippets.el

---
+++ plantuml-snippets.el
@@ -0,0 +1,37 @@
+;;; plantuml-snippets.el --- snippets for plantuml -*- lexical-binding: t -*-
+
+;; Author: hejack0207
+;; Maintainer: hejack0207
+;; Version: 0.0.1
+;; Package-Requires: (yas-snippet)
+;; Homepage: https://github.com/hejack0207/plantuml-snippets.el
+;; Keywords: plantuml snippets
+
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; commentary
+
+;;; Code:
+
+(message "Hello World!")
+
+(provide 'plantuml-snippets)
+
+;;; plantuml-snippets.el ends here
